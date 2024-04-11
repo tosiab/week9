e5b1b2ed115cc31f43ab9dc3308fecd0fa954cf5
tosiab <mtabednarz@gmail.com>
2024-04-11 14:29:54 -0400

    game21doneslay

--- Game21.java
+++ Game21.java
@@ -14,17 +14,30 @@ public class Game21 {
         sum2 = num4+num5+num6;
         Scanner input = new Scanner(System.in);
         System.out.println("You have a "+num1+" and a "+num2+" for a total of "+sum1+".");
-        System.out.print("Enter a 1 if you want another card or any other number for no card.");
+        System.out.print("Enter a 1 if you want another card or any other number for no card. ");
         newcard = input.nextInt();
         if (newcard == 1){
             num3 = (int)(10*Math.random())+1;
             sum1 = sum1 +num3;
             System.out.println("You got a "+num3+" for a total of "+sum1+".");
         }
-        System.out.println("The dealer has a "+num4+", "+num5+", and "+num6+"for a total of "+sum2+".");
+        System.out.println("The dealer has a "+num4+", "+num5+", and "+num6+" for a total of "+sum2+".");
         if(sum1>21){
             System.out.println("Player loses.");
         }
+        else if(sum2>21){
+            System.out.println("Player wins.");
+        }
+        else if(sum1>sum2){
+            System.out.println("Player wins.");
+        }
+        else if(sum2>sum1){
+            System.out.println("Dealer wins.");
+        }
+        else{
+            System.out.println("Tie.");
+        }
+        
 
 
 
